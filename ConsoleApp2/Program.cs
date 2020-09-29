f8d85fc3d533bbbf06b79c159df8452b71955b1c
Марина Діденко <didenko_m@donnu.edu.ua>
2020-09-29 10:07:42 +0300

    задача 2

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -10,6 +10,21 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
+            try
+            {
+                Console.WriteLine($"Введите переменную типа int");
+                int a = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine($"Введите переменную типа double");
+                double b = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine($"Введите пеменную типа long");
+                long c = Convert.ToInt64(Console.ReadLine());
+                Console.WriteLine("a={0}, b={1}, c={2}", a, b, c);
+            }
+            catch
+            {
+                Console.WriteLine($"Неравильно веденная переменная");
+            }
+            Console.ReadKey();
         }
     }
 }
